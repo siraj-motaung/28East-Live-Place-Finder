6d7561ecbd9f17a6d3f8173863ac7a016c65b822
Siraj <sirajmotaung@gmail.com>
2026-08-04 23:42:14 +0200

    created models file for database management

---
+++ models/user.py
@@ -0,0 +1,14 @@
+from database import get_connection
+
+def get_users(id: int):
+
+    conn = get_connection("users")
+
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM users WHERE id=%s", (id,))
+        user = cursor.fetchone()
+
+    conn.close()
+
+    return user
+
