0bf5103809584cbb182536d8de5f1feeefb92790
Siraj <sirajmotaung@gmail.com>
2026-08-04 20:22:55 +0200

    added database config variables

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ import os
 
 class Config:
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+    DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+    DB_PORT = int(os.getenv("DB_PORT", 3306))
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_NAME = os.getenv("DB_NAME", "")
+
